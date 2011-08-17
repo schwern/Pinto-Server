493aa57eccd934795b4561a8b18790de25da1be4
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-17 00:09:05 -0700

    A whole new server, based on Dancer.

--- lib/Pinto/Server.pm
+++ lib/Pinto/Server.pm
@@ -1,57 +1,102 @@
 package Pinto::Server;
 
 # ABSTRACT: Web interface to a Pinto repository
-use Moose;
 
-use CGI::Application::Server;
+use Carp;
+use Data::Dumper;
+use Path::Class;
+use File::Temp;
 
 use Pinto;
+use Pinto::Config;
 use Pinto::Logger;
-use Pinto::Server::Dispatch;
+
+use Dancer ':syntax';
 
 #-----------------------------------------------------------------------------
 
 # VERSION
 
 #-----------------------------------------------------------------------------
+# These are persistent variables!
 
-with qw( MooseX::Daemonize );
+my $config = Pinto::Config->new();
+my $logger = Pinto::Logger->new(config => $config);
+my $pinto  = Pinto->new(logger => $logger, config => $config);
 
-#-----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 
-has config   => (
-    is       => 'ro',
-    isa      => 'Pinto::Server::Config',
-    required => 1,
-);
+post '/add' => sub {
 
+    my $author = param('author')
+      or (status 500 and return 'No author supplied');
 
-has '+pidbase' => (
-    default  => sub { $_[0]->config->config_file->dir() },
-);
+    my $dist   = upload('dist')
+      or (status 500 and return 'No dist file supplied');
 
-#-----------------------------------------------------------------------------
+    my $tempdir = dir( File::Temp::tempdir(CLEANUP=>1) );
+    my $dist_file = $tempdir->file( $dist->basename() );
+    $dist->copy_to( $dist_file );
 
-after start => sub {
-    my $self = shift;
-    return unless $self->is_daemon();
+    eval {$pinto->add(dists => $dist_file, author => $author); 1}
+        or (status 500 and return $@);
 
-    my $server = CGI::Application::Server->new( $self->config->port() );
-    $server->document_root( $self->config->local() );
+    status 200;
+    return "SUCCESS\n";
 
-    my $logger = Pinto::Logger->new( config => $self->config() );
-    my $pinto = Pinto->new( config => $self->config(), logger => $logger );
-    my $dispatch = Pinto::Server::Dispatch->new( pinto => $pinto );
-    $server->entry_points( {'/action' => $dispatch} );
-
-    return $server->run();
 };
 
 #----------------------------------------------------------------------------
 
-__PACKAGE__->meta->make_immutable();
+post '/remove' => sub {
+
+    my $author  = param('author')  or return _error('No author supplied');
+    my $package = param('package') or return _error('No package supplied');
+
+    eval {$pinto->remove(packages => $package, author => $author); 1}
+        or (status 500 and return $@);
+
+    status 200;
+    return "SUCCESS\n";
+};
 
 #----------------------------------------------------------------------------
+
+post '/list' => sub {
+
+    my $buffer = '';
+    eval {$pinto->list(out => \$buffer); 1}
+        or (status 500 and return $@);
+
+    status 200;
+    return $buffer;
+
+};
+
+#----------------------------------------------------------------------------
+
+get '/' => sub {
+
+    status 200;
+    return "OK";
+};
+
+#----------------------------------------------------------------------------
+
+get '/authors/**' => sub {
+     my $file =  file( $config->local(), request->uri() );
+     return send_file( $file, system_path => 1 );
+};
+
+#----------------------------------------------------------------------------
+
+get '/modules/**' => sub {
+     my $file =  file( $config->local(), request->uri() );
+     return send_file( $file, system_path => 1 );
+};
+
+#----------------------------------------------------------------------------
+
 1;
 
 __END__
