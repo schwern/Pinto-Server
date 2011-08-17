6c5cdc26aabf1a0300420e7e3befde0d051a7679
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-08-17 01:00:18 -0700

    Adding documentation.

--- lib/Pinto/Server.pm
+++ lib/Pinto/Server.pm
@@ -2,6 +2,9 @@ package Pinto::Server;
 
 # ABSTRACT: Web interface to a Pinto repository
 
+use strict;
+use warnings;
+
 use Carp;
 use Data::Dumper;
 use Path::Class;
@@ -96,7 +99,60 @@ get '/modules/**' => sub {
 };
 
 #----------------------------------------------------------------------------
-
 1;
 
 __END__
+
+=head1 DESCRIPTION
+
+You probably want to look at L<pinto-server> first.
+
+L<Pinto::Server> is a web API to a L<Pinto> repository.  Using this
+interface, remote clients (like L<pinto-remote>) can add
+distributions, remove packages, and list the contents of the Pinto
+repository.  In addition, L<Pinto::Server> serves the entire contents
+of your repository, so you can use it as the source of distributions
+for L<cpan> or L<cpanm>.
+
+Before running L<Pinto::Server> you must first create a Pinto
+repository.  See L<pinto-admin> for directions on that.  Once you have
+a repository, the easiest way to run L<Pinto::Server> is like this:
+
+  $> pinto-server [OPTIONS]
+
+L<Pinto::Server> is also PSGI compatible, so you can run it under
+L<Plack> like this:
+
+  $> plackup [OPTIONS] /path/to/pinto-server
+
+=head1 CONFIGURATION
+
+L<Pinto::Server> automatically uses your L<Pinto> configuration file
+which is usually at F<$HOME/.pinto/config.ini>.  Or you can set the
+C<PERL_PINTO> environment variable to point to another location.
+
+No additional configuration is required beyond what L<Pinto> itself
+uses.  However, L<Pinto::Server> will always silently force the
+C<nocommit> and C<noinit> parameters to 0.  Also, the C<author>
+parameter is meaningless to L<Pinto::Server> because clients all
+required to provide an author for any C<add> or C<remove> operations.
+
+=head1 CAVEATS
+
+If you are running L<Pinto::Server> and have configured Pinto to use a
+VCS-based store, such as L<Pinto::Store::Svn> or L<Pinto::Store::Git>,
+then you must not mess with the VCS directly (at least not the VCS
+directories that Pinto is using).  This is because L<Pinto::Server>
+only initializes the working copy of the Pinto repository at startup.
+Thereafter, it assumes that it is the only actor that affects its part
+of the VCS.  If you start
+
+=head1 LIMITATIONS
+
+L<Pinto::Server> speaks HTTP, but does not actually serve HTML.  At
+the moment, is geared toward command-line tools like L<pinto-client>
+so it just returns plain text.  This will probably change as
+L<Pinto::Server> evolves into a real web application.
+
+=cut
+
